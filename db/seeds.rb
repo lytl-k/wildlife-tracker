f1f536c3d734a7dabd16efc333376fd16412c231
Kobus Kemp <kobusk@Kobuss-MacBook-Pro.local>
2020-03-04 09:44:08 +0200

    chore: Adds basic seeds for regions and animal types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+AnimalType.where(name: 'Aquatic').first_or_create(name: 'Aquatic')
+AnimalType.where(name: 'Land').first_or_create(name: 'Land')
+AnimalType.where(name: 'Airborn').first_or_create(name: 'Airborn')
+
+Region.where(name: 'This').first_or_create(name: 'This')
+Region.where(name: 'That').first_or_create(name: 'That')
+Region.where(name: 'Another').first_or_create(name: 'Another')
+Region.where(name: 'Outside').first_or_create(name: 'Outside')
+Region.where(name: 'OtherSide').first_or_create(name: 'OtherSide')
